ebf0317ff5f7769baa46054c898f9ba0a60c2477
cindyita <candy95wh@gmail.com>
2025-07-06 01:58:31 -0600

    responsive fix

--- views/home.php
+++ views/home.php
@@ -144,7 +144,7 @@
               </select>
             </div>
 
-            <div>
+            <div class="w-full md:w-1/4">
               <select
                 x-model="form.extras"
                 class="input border border-blue-300 rounded-full focus:outline-none focus:ring-4 focus:ring-blue-300 px-4 py-2 transition w-full" title="Invitados extras (Solo tu o tu+cuantos)"
